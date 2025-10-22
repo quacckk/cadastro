1165c62c5d28701811389f2c35466e30af332bf9
Victor Lima <joaozinhobaodebriga@gmail.com>
2025-10-22 17:06:18 -0300

    Add files via upload
    código finalizado

---
+++ usuario.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = input("Olá {}, agora digite a sua idade: ".format(nome))
+
+print("Nome: {}".format(nome))
+print("Nome: {}. Idade: {}".format(nome,idade))
